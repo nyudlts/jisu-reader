139a018ea810f1295420b7911448afef3f1e19ef
Patrick Keating <pkeating@bluefirereader.com>
2025-03-28 07:58:43 -0700

    update NYUReaderHeader

--- src/components/NYUReaderHeader.tsx
+++ src/components/NYUReaderHeader.tsx
@@ -1,20 +1,22 @@
 import React, { useCallback, useRef } from "react";
-import Image from "next/image";
 
 import { RSPrefs } from "@/preferences";
 import Locale from "../resources/locales/en.json";
 
 import { ActionKeys, IActionsItem, IActionsMapObject } from "@/models/actions";
-import { ThemeKeys } from "@/models/theme";
 
 import readerStateStyles from "./assets/styles/readerStates.module.css";
-import readerHeaderStyles from "./assets/styles/nyuReaderHeader.module.css";
+import readerHeaderStyles from "./assets/styles/readerHeader.module.css";
 
-import { FullscreenAction } from "./NYUFullscreenAction";
+import { FullscreenAction } from "./FullscreenAction";
 import { JumpToPositionAction } from "./JumpToPositionAction";
-import { SettingsAction, SettingsActionContainer } from "./NYUSettingsAction";
-import { TocAction, TocActionContainer } from "./NYUTocAction";
+import { LayoutStrategyAction, LayoutStrategiesActionContainer } from "./LayoutStrategyAction";
+import { SettingsAction, SettingsActionContainer } from "./SettingsAction";
+import { TocAction, TocActionContainer } from "./TocAction";
 import { NYUSearchAction, NYUSearchContainer } from "./NYUSearchAction";
+import { NYUBookInfoAction, NYUBookInfoActionContainer } from "./NYUBookInfoAction";
+import { NYUAboutAction, NYUAboutActionContainer } from "./NYUAboutAction";
+import { NYUPageListAction, NYUPageListActionContainer } from "./NYUPageListAction";
 import { RunningHead } from "./RunningHead";
 import { ActionsWithCollapsibility } from "./ActionsWithCollapsibility";
 
@@ -30,6 +32,10 @@ const ActionsMap: { [key in ActionKeys]: IActionsMapObject } = {
   [ActionKeys.jumpToPosition]: {
     trigger: JumpToPositionAction
   },
+  [ActionKeys.layoutStrategy]: {
+    trigger: LayoutStrategyAction,
+    container: LayoutStrategiesActionContainer
+  },
   [ActionKeys.settings]: {
     trigger: SettingsAction,
     container: SettingsActionContainer
@@ -41,30 +47,26 @@ const ActionsMap: { [key in ActionKeys]: IActionsMapObject } = {
   [ActionKeys.nyuSearch]: {
     trigger: NYUSearchAction,
     container: NYUSearchContainer
+  },
+  [ActionKeys.nyuBookInfo]: {
+    trigger: NYUBookInfoAction,
+    container: NYUBookInfoActionContainer
+  },
+  [ActionKeys.nyuAbout]: {
+    trigger: NYUAboutAction,
+    container: NYUAboutActionContainer
+  },
+  [ActionKeys.nyuPageList]: {
+    trigger: NYUPageListAction,
+    container: NYUPageListActionContainer
   }
 }
 
 export const ReaderHeader = () => {
+  const isFXL = useAppSelector(state => state.publication.isFXL);
   const actionsOrder = useRef(RSPrefs.actions.displayOrder);
   const isImmersive = useAppSelector(state => state.reader.isImmersive);
   const isHovering = useAppSelector(state => state.reader.isHovering);
-  const scheme = useAppSelector((state) => state.theming.colorScheme);
-  const theme = useAppSelector((state) => state.theming.theme);
-
-  // Switch the header based on the theme, auto = NYU Theme 
-  let headerID = (theme === ThemeKeys.auto) ? "top-bar-nyu" : "top-bar";
-  const headerClassName = (theme === ThemeKeys.auto) ? readerHeaderStyles.nyuHeader : readerHeaderStyles.header;
-  let logoSrc = "/nyu-logo-dark.svg";
-
-  // Switch the logo based on the theme, auto = NYU Theme 
-  if (theme === ThemeKeys.auto) {
-    headerID = "nyu-top-bar";
-    logoSrc = "/nyu-logo.svg";
-  } else if (theme === ThemeKeys.contrast1) {
-    logoSrc = "/nyu-logo-yellow.svg";
-  } else if (theme === ThemeKeys.dark || theme === ThemeKeys.contrast2) {
-    logoSrc = "/nyu-logo.svg";
-  }
 
   const dispatch = useAppDispatch();
 
@@ -90,28 +92,27 @@ export const ReaderHeader = () => {
     const actionsItems: IActionsItem[] = [];
 
     actionsOrder.current.map((key: ActionKeys) => {
-      actionsItems.push({
-        Trigger: ActionsMap[key].trigger,
-        Container: ActionsMap[key].container,
-        key: key
-      });
+      if (key !== ActionKeys.layoutStrategy || !isFXL) {
+        actionsItems.push({
+          Trigger: ActionsMap[key].trigger,
+          Container: ActionsMap[key].container,
+          key: key
+        });
+      }
     });
     
     return actionsItems;
-  }, []);
+  }, [isFXL]);
 
   return (
     <>
     <header 
-      className={ classNames(headerClassName, handleClassNameFromState()) } 
-      id={ headerID } 
+      className={ classNames(readerHeaderStyles.header, handleClassNameFromState()) } 
+      id="top-bar" 
       aria-label={ Locale.reader.app.header.label } 
       onMouseEnter={ setHover } 
       onMouseLeave={ removeHover }
     >
-      <div className={ readerHeaderStyles.logoWrapper }>
-        <Image src={logoSrc} alt="NYU Libraries Logo" width={200} height={60} />
-      </div>
       <RunningHead syncDocTitle={ true } />
       
       <ActionsWithCollapsibility 
