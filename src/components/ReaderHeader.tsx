f27e6bf508b854e44f52f714f39540c41b4a3bdf
Patrick Keating <pkeating@bluefirereader.com>
2025-04-03 12:28:23 -0700

    NYU style, icon and h1 for header

--- src/components/ReaderHeader.tsx
+++ src/components/ReaderHeader.tsx
@@ -1,4 +1,6 @@
 import React, { useCallback, useRef } from "react";
+import Image from "next/image";
+import { ThemeKeys } from "@/models/theme";
 
 import { RSPrefs } from "@/preferences";
 import Locale from "../resources/locales/en.json";
@@ -67,6 +69,23 @@ export const ReaderHeader = () => {
   const actionsOrder = useRef(RSPrefs.actions.displayOrder);
   const isImmersive = useAppSelector(state => state.reader.isImmersive);
   const isHovering = useAppSelector(state => state.reader.isHovering);
+  const scheme = useAppSelector((state) => state.theming.colorScheme);
+  const theme = useAppSelector((state) => state.theming.theme);
+
+  // NYU Switch the header based on the theme, auto = NYU Theme 
+  let headerID = (theme === ThemeKeys.auto) ? "top-bar-nyu" : "top-bar";
+  const headerClassName = (theme === ThemeKeys.auto) ? readerHeaderStyles.nyuHeader : readerHeaderStyles.header;
+  let logoSrc = "/nyu-logo-dark.svg";
+
+  // Switch the logo based on the theme, auto = NYU Theme 
+  if (theme === ThemeKeys.auto) {
+    headerID = "top-bar-nyu";
+    logoSrc = "/nyu-logo.svg";
+  } else if (theme === ThemeKeys.contrast1) {
+    logoSrc = "/nyu-logo-yellow.svg";
+  } else if (theme === ThemeKeys.dark || theme === ThemeKeys.contrast2) {
+    logoSrc = "/nyu-logo.svg";
+  }
 
   const dispatch = useAppDispatch();
 
@@ -107,12 +126,15 @@ export const ReaderHeader = () => {
   return (
     <>
     <header 
-      className={ classNames(readerHeaderStyles.header, handleClassNameFromState()) } 
-      id="top-bar" 
+      className={ classNames(readerHeaderStyles.nyuHeader, handleClassNameFromState()) } 
+      id={ headerID }  
       aria-label={ Locale.reader.app.header.label } 
       onMouseEnter={ setHover } 
       onMouseLeave={ removeHover }
     >
+      <div className={ readerHeaderStyles.logoWrapper }>
+        <Image src={logoSrc} alt="NYU Libraries Logo" width={200} height={60} />
+      </div>
       <RunningHead syncDocTitle={ true } />
       
       <ActionsWithCollapsibility 
